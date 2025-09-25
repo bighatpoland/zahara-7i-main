fa187544b45d644a66417e4d310329d09edd058c
Konstancja <bighatpoland@gmail.com>
2025-09-25 11:57:22 +0000

    polaczenie do builder

--- src/app/builder/page.tsx
+++ src/app/builder/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { builder, BuilderComponent } from "@builder.io/react";
 
-// klucz z .env.local
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
 export default function BuilderPage() {
